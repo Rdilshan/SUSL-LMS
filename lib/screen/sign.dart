7a39716081d79763a40ce5977266927a419a4994
disara7 <129250994+disara7@users.noreply.github.com>
2023-04-25 18:20:10 +0530

    colors

--- lib/screen/sign.dart
+++ lib/screen/sign.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 class Sign extends StatefulWidget {
   const Sign({super.key});
 
@@ -14,50 +13,40 @@ class _SignState extends State<Sign> {
     return Scaffold(
       body: Container(
         color: Colors.white,
-
         child: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-
               Image.asset(
                 'Assets/images/logo.png',
                 width: 200,
                 height: 100,
               ),
-
               const SizedBox(
                 height: 10,
               ),
-              
-
               const SizedBox(
                 width: 350,
                 child: TextField(
-                decoration: InputDecoration(
-                  hintText: 'USERNAME',
-                  border: OutlineInputBorder(),
-                ),
+                  decoration: InputDecoration(
+                    hintText: 'USERNAME',
+                    border: OutlineInputBorder(),
+                  ),
                 ),
               ),
-              
-            const SizedBox(
+              const SizedBox(
                 height: 10,
               ),
-
               const SizedBox(
                 width: 350,
                 child: TextField(
                   obscureText: true,
                   decoration: InputDecoration(
-                  hintText: 'PASSWORD',
-                  border: OutlineInputBorder(),
-                ),
+                    hintText: 'PASSWORD',
+                    border: OutlineInputBorder(),
+                  ),
                 ),
               ),
-
-             
-
               const Text(
                 'FORGOT USERNAME OR PASSWORD?',
                 style: TextStyle(
@@ -66,16 +55,14 @@ class _SignState extends State<Sign> {
                   fontFamily: 'OpenSans',
                 ),
               ),
-
               ElevatedButton(
-                onPressed:null,
+                onPressed: null,
                 style: ButtonStyle(
-                  backgroundColor:
-                      MaterialStateProperty.all<Color>(Colors.blue),
+                  backgroundColor: MaterialStateProperty.all<Color>(
+                      Color.fromARGB(255, 81, 24, 24)),
                   fixedSize:
                       MaterialStateProperty.all<Size>(const Size(250, 40)),
                 ),
-
                 child: const Text(
                   'LOGIN',
                   style: TextStyle(
@@ -86,7 +73,6 @@ class _SignState extends State<Sign> {
                   ),
                 ),
               ),
-
               const Text(
                 'SOME COURSES MAY ALLOW GUEST ACCESS',
                 style: TextStyle(
@@ -95,16 +81,14 @@ class _SignState extends State<Sign> {
                   fontFamily: 'OpenSans',
                 ),
               ),
-
               ElevatedButton(
-                onPressed:null,
+                onPressed: null,
                 style: ButtonStyle(
-                  backgroundColor:
-                      MaterialStateProperty.all<Color>(Colors.blue),
+                  backgroundColor: MaterialStateProperty.all<Color>(
+                      Color.fromARGB(255, 81, 24, 24)),
                   fixedSize:
                       MaterialStateProperty.all<Size>(const Size(250, 40)),
                 ),
-
                 child: const Text(
                   'LOGIN AS A GUEST',
                   style: TextStyle(
@@ -115,7 +99,6 @@ class _SignState extends State<Sign> {
                   ),
                 ),
               ),
-
             ],
           ),
         ),
---
+++ lib/submitpage.dart
@@ -0,0 +1,6 @@
+//class CartPage extends StatefulWidget {
+  //const CartPage({Key? key}) : super(key: key);
+//
+  //@override
+  //State<>
+//}
