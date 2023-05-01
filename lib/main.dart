41ab73f98dc1e46ed2d1d3840d40bad0c175ebdd
disara7 <129250994+disara7@users.noreply.github.com>
2023-05-01 13:50:41 +0530

    edits

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,14 @@
 import 'package:flutter/material.dart';
 
+<<<<<<< Updated upstream
+=======
+import 'package:flutter_application_5/notifications/notifications.dart';
+import 'package:flutter_application_5/submit_assignment/assignment.dart';
+
+import 'Src/theme/theme.dart';
+>>>>>>> Stashed changes
 import 'screen/splash.dart';
+import 'notifications/notifications.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,7 +19,15 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+<<<<<<< Updated upstream
     return const MaterialApp(
+=======
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: LMStheme.lightTheme,
+      darkTheme: LMStheme.darkTheme,
+      themeMode: ThemeMode.system,
+>>>>>>> Stashed changes
       home: Splash(),
     );
   }
